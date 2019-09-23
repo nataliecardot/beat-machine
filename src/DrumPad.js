dade96e38372f3ae0871f5071d454b26d15529d1
Natalie-Cardot <nataliecardot@gmail.com>
2019-09-22 23:19:55 -0700

    Destructure props

--- src/DrumPad.js
+++ src/DrumPad.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
-const DrumPad = (props) => {
+// Destructuring used to pass props (see https://www.youtube.com/watch?v=CFUysPqYC4Y)
+// 'props' is the convention for the first argument passed to component, but could be named anything
+// Effectively, { letter } = arguments[0])
+const DrumPad = ({ id, letter }) => {
 
   return (
-    <div id={props.id}>
-      <p>{props.letter}</p>
+    <div id={id}>
+      <p>{letter}</p>
     </div>
   );
 }
