2125f390ee1bb95bb1a6b58fede65b0d0a28dc8d
Natalie-Cardot <nataliecardot@gmail.com>
2019-09-26 16:57:23 -0700

    Updates

---
+++ precache-manifest.1b365ec7f009d4c40a74410ef32959e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ea1116d336934f4fb80f099e8b2fa74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e0eedb34ea606c1da77",
+    "url": "/static/css/main.c43b272c.chunk.css"
+  },
+  {
+    "revision": "6a1f924751af5aeeb0e5",
+    "url": "/static/js/2.cde5230b.chunk.js"
+  },
+  {
+    "revision": "3e0eedb34ea606c1da77",
+    "url": "/static/js/main.8ccb09b2.chunk.js"
+  },
+  {
+    "revision": "04716cc15911d94f49d9",
+    "url": "/static/js/runtime-main.0016c3c5.js"
+  }
+]);
