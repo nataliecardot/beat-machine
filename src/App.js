ec7c70d4f75994ed0450314b976b9e5f49a555b5
Natalie-Cardot <nataliecardot@gmail.com>
2019-09-26 16:15:06 -0700

    feat: Play sounds on keydown

--- src/App.js
+++ src/App.js
@@ -6,58 +6,49 @@ const sounds = [
   {
     id: 'snare',
     letter: 'Q',
-    keycode: 81,
     src:
       'https://www.myinstants.com/media/sounds/snare.mp3'
   },
   {
     id: 'bass boost',
     letter: 'W',
-    keycode: 87,
     src: 'https://www.myinstants.com/media/sounds/bass-boost_oJjgiYr.mp3'
   },
   {
     id: 'bass kick',
     letter: 'E',
-    keycode: 69,
     src: 'http://dight310.byu.edu/media/audio/FreeLoops.com/3/3/Free%20Kick%20Sample%203-888-Free-Loops.com.mp3'
   },
   {
     id: 'tom-tom',
     letter: 'A',
-    keycode: 65,
     src: 'http://www.denhaku.com/r_box/linn/midtom.wav'
   },
   {
     id: 'shotgun reload',
     letter: 'S',
-    keycode: 83,
     src:
       'http://david.guerrero.free.fr/Effects/ShotgunReload.wav'
   },
   {
     id: 'cymbal',
     letter: 'D',
-    keycode: 68,
     src: 'https://www.myinstants.com/media/sounds/vdub1-crash-18.mp3'
   },
   {
     id: 'closed hi-hat',
     letter: 'Z',
-    keycode: 90,
     src:
       'https://www.myinstants.com/media/sounds/behemoth-closed-26_4fwxFnC.mp3'
   },
   {
     id: 'clap',
     letter: 'X',
-    keycode: 88,
     src: 'https://www.myinstants.com/media/sounds/clap.mp3'
   },
   {
     id: 'open hi-hat',
     letter: 'C',
-    keycode: 67,
     src:
       'https://www.myinstants.com/media/sounds/vdub1-op-hihat-010_5elr79H.mp3'
   }
--- src/DrumPad.js
+++ src/DrumPad.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // Destructuring used to pass props (see https://www.youtube.com/watch?v=CFUysPqYC4Y)
 const DrumPad = ({ id, letter, src, handleDisplay }) => {
 
   let audio = React.createRef();
 
+  // Effect Hook. Like componentDidMount, componentDidUpdate, and componentWillUnmount lifecycle methods combined. Runs after each render
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeydown);
+
+    // Cleanup (adding and removing is closely related, so a feature of useEffect is that this logic can be kept together. Every effect can optionally return a cleanup function that cleans up after it)
+    return () => document.removeEventListener('keydown', handleKeydown);
+  });
+
   const handleClick = () => {
     audio.current.play();
-    audio.current.currentTime = 0;
     handleDisplay(id);
+  }
+
+  const handleKeydown = e => {
+    if (e.keyCode === letter.charCodeAt()) {
+      audio.current.play();
+      handleDisplay(id);
+    }
   }
 
   return (
